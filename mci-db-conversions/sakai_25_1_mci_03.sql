61c881f3470e6f74fe0cc7dbaf219c1f6f82fce7
Sebastian Riemer <sebastian.riemer@mci.edu>
2026-04-22 08:37:08 +0200

    Rename existing JupyterHub Tool

---
+++ mci-db-conversions/sakai_25_1_mci_03.sql
@@ -0,0 +1,6 @@
+-- In order to distinguish the current external tool "JupyterHub" from the new variant we want better naming
+
+update sakai.lti_tools set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update sakai.lti_content set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update sakai.sakai_site_tool set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update sakai.sakai_site_page set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
