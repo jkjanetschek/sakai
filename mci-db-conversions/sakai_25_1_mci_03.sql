186b9d89a4f7f92b1936c4f6a73b791b8e6d7891
Sebastian Riemer <sebastian.riemer@mci.edu>
2026-08-10 14:32:06 +0200

    Remove schema prefix

--- mci-db-conversions/sakai_25_1_mci_03.sql
+++ mci-db-conversions/sakai_25_1_mci_03.sql
@@ -1,9 +1,9 @@
 -- In order to distinguish the current external tool "JupyterHub" from the new variant we want better naming
 
-update sakai.lti_tools set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
-update sakai.lti_content set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
-update sakai.sakai_site_tool set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
-update sakai.sakai_site_page set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update lti_tools set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update lti_content set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update sakai_site_tool set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
+update sakai_site_page set title = 'JupyterHub (Legacy)' where title = 'JupyterHub';
 
 INSERT INTO mci_schema_migrations (name, executed_at)
 VALUES ('sakai_25_1_mci_03.sql', NOW(6));
